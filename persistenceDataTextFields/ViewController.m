5f99400456a938bbb847e8cf26138435d62afea7
josue rangel <josrangel@hotmail.com>
2020-10-26 17:07:02 -0600

    Se agrega la funcionalidad y el diseño al Storyboard

--- persistenceDataTextFields/ViewController.m
+++ persistenceDataTextFields/ViewController.m
@@ -8,7 +8,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong,nonatomic) IBOutletCollection(UITextField) NSArray *linefields;
 @end
 
 @implementation ViewController
@@ -16,6 +16,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSString *filepath = [self dataFilePath];
+    if([[NSFileManager defaultManager] fileExistsAtPath:filepath]){
+        NSArray *array = [[NSArray alloc] initWithContentsOfFile:filepath];
+        for (int i = 0; i<4; i++) {
+            UITextField *theField = self.linefields[i];
+            theField.text = array[i];
+        }
+    }
+    
+    UIApplication *app = [UIApplication sharedApplication];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationWillResignActive:)  name:UIApplicationWillResignActiveNotification object:app];
+    
+    
+}
+
+-(void)applicationWillResignActive:(NSNotification *)notification{
+    NSString *filePath = [self dataFilePath];
+    NSArray *array = [self.linefields valueForKey:@"text"];
+    [array writeToFile:filePath atomically:YES];
+}
+
+-(NSString *)dataFilePath{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    return [documentsDirectory stringByAppendingPathComponent:@"data.plist"];
 }
 
 
